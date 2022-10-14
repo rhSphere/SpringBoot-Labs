4d77be2c78df7d5088fda2317d6703764d08fa82
ludepeng <ludepeng@bytedance.com>
2022-10-14 23:48:30 +0800

    修改配置

--- lab-55/lab-55-mapstruct-demo/src/main/java/cn/iocoder/springboot/lab55/mapstructdemo/bo/UserDetailBO.java
+++ lab-55/lab-55-mapstruct-demo/src/main/java/cn/iocoder/springboot/lab55/mapstructdemo/bo/UserDetailBO.java
@@ -4,6 +4,17 @@ public class UserDetailBO {
 
     private Integer userId;
 
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    private String username;
+
+
     public Integer getUserId() {
         return userId;
     }
